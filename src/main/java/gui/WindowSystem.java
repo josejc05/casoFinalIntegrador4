317fd8a1ad9fcb4b5d65467680cca9557a7207f3
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-22 16:08:29 +0100

    .

--- src/main/java/gui/WindowSystem.java
+++ src/main/java/gui/WindowSystem.java
@@ -1,4 +1,12 @@
 package gui;
 
+import javax.swing.*;
+
 public class WindowSystem {
+    public static void createWindow(String title) {
+        JFrame frame = new JFrame(title);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+    }
 }
