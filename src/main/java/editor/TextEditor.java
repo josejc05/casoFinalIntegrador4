6d2f68924abd23748c2f3cbc2d0a0f9ebe83e222
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-22 16:10:15 +0100

    .

--- src/main/java/editor/TextEditor.java
+++ src/main/java/editor/TextEditor.java
@@ -1,4 +1,23 @@
 package editor;
 
-public class TextEditor {
+import javax.swing.*;
+import java.awt.*;
+
+public class TextEditor extends JFrame {
+    private JTextArea textArea;
+
+    public TextEditor() {
+        textArea = new JTextArea();
+        add(new JScrollPane(textArea), BorderLayout.CENTER);
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void setText(String text) {
+        textArea.setText(text);
+    }
+
+    public String getText() {
+        return textArea.getText();
+    }
 }
