8665469e1d946c88a482d1222730767ebbfe85b2
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-22 16:09:16 +0100

    .

--- src/main/java/gui/MouseTracker.java
+++ src/main/java/gui/MouseTracker.java
@@ -1,4 +1,18 @@
 package gui;
 
-public class MouseTracker {
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.*;
+
+public class MouseTracker extends JLabel {
+    public MouseTracker() {
+        super("Mouse at (?, ?)");
+        addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                setText("Mouse at (" + e.getX() + ", " + e.getY() + ")");
+            }
+        });
+    }
 }
