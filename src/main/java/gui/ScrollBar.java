603201493100ea42e6ec7037eff7d0725d079cb0
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-22 16:08:47 +0100

    .

--- src/main/java/gui/ScrollBar.java
+++ src/main/java/gui/ScrollBar.java
@@ -1,4 +1,11 @@
 package gui;
 
+import javax.swing.*;
+
 public class ScrollBar {
+    public static JScrollPane createScrollBar(JComponent component) {
+        JScrollPane scrollPane = new JScrollPane(component);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        return scrollPane;
+    }
 }
