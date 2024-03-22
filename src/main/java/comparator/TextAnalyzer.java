67c75c1e45c48ce3b8f8dba48e6c2909300046e7
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-22 16:11:45 +0100

    .

--- src/main/java/comparator/TextAnalyzer.java
+++ src/main/java/comparator/TextAnalyzer.java
@@ -1,4 +1,18 @@
 package comparator;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TextAnalyzer {
+
+    public static Map<String, Integer> wordFrequency(String text) {
+        Map<String, Integer> frequencyMap = new HashMap<>();
+        String[] words = text.toLowerCase().split("\\W+");
+
+        for (String word : words) {
+            frequencyMap.put(word, frequencyMap.getOrDefault(word, 0) + 1);
+        }
+
+        return frequencyMap;
+    }
 }
