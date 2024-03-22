f44596aa97da614cb29d74722edf5412b41fd1e3
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-22 16:10:36 +0100

    .

--- src/main/java/editor/DocumentManager.java
+++ src/main/java/editor/DocumentManager.java
@@ -1,4 +1,18 @@
 package editor;
 
+import utils.FileUtils;
+
+import java.io.IOException;
+import java.util.List;
+
 public class DocumentManager {
+
+    public static void saveDocument(String filePath, String content) throws IOException {
+        FileUtils.writeFile(filePath, List.of(content.split("\n")));
+    }
+
+    public static String openDocument(String filePath) throws IOException {
+        List<String> lines = FileUtils.readFile(filePath);
+        return String.join("\n", lines);
+    }
 }
