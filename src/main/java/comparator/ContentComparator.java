ff7e1000811ed536e3e91fec5515e3b1fb3e2ea3
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-22 16:12:03 +0100

    .

--- src/main/java/comparator/ContentComparator.java
+++ src/main/java/comparator/ContentComparator.java
@@ -1,4 +1,13 @@
 package comparator;
 
+import java.util.Map;
+
 public class ContentComparator {
+
+    public static boolean compareContent(String text1, String text2) {
+        Map<String, Integer> frequencyMap1 = TextAnalyzer.wordFrequency(text1);
+        Map<String, Integer> frequencyMap2 = TextAnalyzer.wordFrequency(text2);
+
+        return frequencyMap1.equals(frequencyMap2);
+    }
 }
