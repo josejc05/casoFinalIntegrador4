a1659489fb98ff0ed01d1b3cea9d52a7b33878c0
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-22 16:06:17 +0100

    .

--- src/main/java/search/WordSearcher.java
+++ src/main/java/search/WordSearcher.java
@@ -1,4 +1,23 @@
 package search;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class WordSearcher {
+
+    public static int countOccurrences(String filePath, String word) throws IOException {
+        int count = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                int index = 0;
+                while ((index = line.indexOf(word, index)) != -1) {
+                    count++;
+                    index += word.length();
+                }
+            }
+        }
+        return count;
+    }
 }
