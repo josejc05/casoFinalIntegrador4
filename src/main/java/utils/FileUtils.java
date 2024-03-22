482aea0fc588c5f044ab4671348650efe5d0ebf1
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-22 16:04:46 +0100

    .

--- src/main/java/utils/FileUtils.java
+++ src/main/java/utils/FileUtils.java
@@ -1,4 +1,20 @@
 package utils;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class FileUtils {
+
+    public static List<String> readFile(String filePath) throws IOException {
+        Path path = Paths.get(filePath);
+        return Files.readAllLines(path);
+    }
+
+    public static void writeFile(String filePath, List<String> content) throws IOException {
+        Path path = Paths.get(filePath);
+        Files.write(path, content);
+    }
 }
