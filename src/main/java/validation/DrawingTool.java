a72ec92990b86fd0969f2c79a7704bff6a0b46a7
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-22 16:03:31 +0100

    .

--- src/main/java/validation/DrawingTool.java
+++ src/main/java/validation/DrawingTool.java
@@ -1,4 +1,55 @@
 package validation;
 
-public class DrawingTool {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+public class DrawingTool extends JPanel {
+    private Image image;
+    private Graphics2D graphics2D;
+    private int currentX, currentY, oldX, oldY;
+
+    public DrawingTool() {
+        setDoubleBuffered(false);
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                oldX = e.getX();
+                oldY = e.getY();
+            }
+        });
+
+        addMouseMotionListener(new MouseMotionAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                currentX = e.getX();
+                currentY = e.getY();
+
+                if (graphics2D != null) {
+                    graphics2D.drawLine(oldX, oldY, currentX, currentY);
+                    repaint();
+                    oldX = currentX;
+                    oldY = currentY;
+                }
+            }
+        });
+    }
+
+    protected void paintComponent(Graphics g) {
+        if (image == null) {
+            image = createImage(getSize().width, getSize().height);
+            graphics2D = (Graphics2D) image.getGraphics();
+            graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            clear();
+        }
+
+        g.drawImage(image, 0, 0, null);
+    }
+
+    public void clear() {
+        graphics2D.setPaint(Color.white);
+        graphics2D.fillRect(0, 0, getSize().width, getSize().height);
+        graphics2D.setPaint(Color.black);
+        repaint();
+    }
 }
