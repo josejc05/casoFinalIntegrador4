1343ca3d92d0e24824d4aaf45d92a49ed7c18e36
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-22 16:06:41 +0100

    .

--- src/main/java/search/ContactManager.java
+++ src/main/java/search/ContactManager.java
@@ -1,4 +1,20 @@
 package search;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ContactManager {
+    private Map<String, String> contacts = new HashMap<>();
+
+    public void addContact(String name, String email) {
+        contacts.put(name, email);
+    }
+
+    public String getContact(String name) {
+        return contacts.get(name);
+    }
+
+    public void removeContact(String name) {
+        contacts.remove(name);
+    }
 }
