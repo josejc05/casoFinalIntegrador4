b1cb80df241f5871300872ce2ef872c48b292a7d
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-25 16:51:16 +0100

    .

--- src/main/java/editor/TextEditor.java
+++ src/main/java/editor/TextEditor.java
@@ -24,4 +24,12 @@ public class TextEditor extends JFrame {
     public JTextArea getTextArea() {
         return textArea;
     }
+    public void saveToFile(String filename) throws IOException {
+        Files.write(Paths.get(filename), textArea.getText().getBytes());
+    }
+
+    public void loadFromFile(String filename) throws IOException {
+        String content = new String(Files.readAllBytes(Paths.get(filename)));
+        textArea.setText(content);
+    }
 }
