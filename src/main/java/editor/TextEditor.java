e66ddb83ed70ba37b8e611557d1caea67231cf93
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-25 18:11:07 +0100

    .

--- src/main/java/editor/TextEditor.java
+++ src/main/java/editor/TextEditor.java
@@ -1,19 +1,13 @@
 package editor;
 
 import javax.swing.*;
-import java.awt.*;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
-public class TextEditor extends JFrame {
+public class TextEditor extends JPanel {
     private JTextArea textArea;
 
     public TextEditor() {
         textArea = new JTextArea();
-        add(new JScrollPane(textArea), BorderLayout.CENTER);
-        setSize(500, 500);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(textArea);
     }
 
     public void setText(String text) {
@@ -24,16 +18,5 @@ public class TextEditor extends JFrame {
         return textArea.getText();
     }
 
-    public JTextArea getTextArea() {
-        return textArea;
-    }
-
-    public void saveToFile(String filename) throws IOException {
-        Files.write(Paths.get(filename), textArea.getText().getBytes());
-    }
-
-    public void loadFromFile(String filename) throws IOException {
-        String content = new String(Files.readAllBytes(Paths.get(filename)));
-        textArea.setText(content);
-    }
+    // Implementar los métodos para guardar y cargar desde un archivo...
 }
